3c36a37eb821500d24e5c0bd6a9e57f26e053abf
Keegan Mann <keeganmann@gmail.com>
2011-09-24 18:22:50 +0000

    custom image for goal
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@92 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- GoalBlock.py
+++ GoalBlock.py
@@ -1,4 +1,5 @@
 from block import Block
 
 class GoalBlock(Block):
-    pass
+    def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co):
+        super(GoalBlock, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
