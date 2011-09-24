4ded9417629ab1f26a748150507b36ea19b9bd41
Peterfly <peterqian@hotmail.com>
2011-09-24 14:13:22 +0000

    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@63 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- monster.py
+++ monster.py
@@ -1,34 +1,47 @@
 from elements import *
 
 class Monster(Element):
-    run_velocity = 5
+    run_velocity = 2
     jump_velocity = 5
+    
     
 
     def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co, dx, dy):
         super(Monster, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
         self.x_lim, self.y_lim = dx, dy
-        self.vx = self.run_velocity
-        self.vy = self.jump_velocity
+        self.vx = 2
+        self.vy = 0
         self.x_ori = self.x
         self.y_ori = self.y
 
     def sim(self, time):
         if abs(self.x_ori - self.x) > self.x_lim:
             self.vx = -self.vx
+        self.vy = 0
         super(Monster, self).sim(time)
+
+    def move(self, elem):
+        from terrain import Terrain
+        if isinstance(elem, Terrain):
+            pass
+        super(Monster, self).move(elem)
 
     def collide(self, elem):
         from block import Block
         from player import Player
+        from terrain import Terrain
         if isinstance(elem, Block):
             super(Monster, self).collide(elem)
         elif isinstance(elem, Player):
-            direction = self.touch(elem)
+            self.exist = False
+            """direction = self.touch(elem)
             if direction == 4:
-                self.exit = False
+                self.exist = False
             else:
-                elem.ch_health(-10)
+                elem.ch_health(-10)"""
+        elif isinstance(elem, Terrain):
+            pass
+   
 
     
                 
