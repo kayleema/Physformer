da26998c97eeb0c3f89008f5154bf2a65762512e
Keegan Mann <keeganmann@gmail.com>
2011-09-24 06:09:12 +0000

    Added import statements in TutLevel and changed player to first element
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@14 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- TutLevel.py
+++ TutLevel.py
@@ -1,3 +1,9 @@
+from elements import Element
+from block import Block
+from player import Player
+from terrain import Terrain
+from Level import Level
+
 class TutLevel(Level):
     def __init__(self, width, height):
         super(TutLevel, self).__init__(width, height)
@@ -5,6 +11,7 @@ class TutLevel(Level):
         a = Terrain(5, 5, 10, 1, 0, 0, 10, self, 0.5)
         b =     Box(5, 0,  1, 1, 0, 0,  1, self, 0.5)
         c =  Player(0, 0,  1, 2, 0, 0,  1, self, 0.5)
+        self.add_elem(c)
         self.add_elem(a)
         self.add_elem(b)
-        self.add_elem(c)
+
