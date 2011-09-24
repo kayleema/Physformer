617730f8ba7d4f1885b18ee6c95910832c4cb6ab
Keegan Mann <keeganmann@gmail.com>
2011-09-24 20:34:32 +0000

    added trampoline
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@112 7638e4d6-f48f-4d7e-a304-0c041ff8053c

---
+++ Trampoline.py
@@ -0,0 +1,8 @@
+from terrain import Terrain
+
+class Trampoline(Terrain):
+    def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co):
+        super(Trampoline, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
+        self.graphics = TiledGraphics("img/tramp.png", 40, 40)
+    def collide(self, elem):
+        elem.vy = -18
