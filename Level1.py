ba7fb954a3f32e828cb4eea7d58b6d0cbf6772cb
Ajay Tripathi <chausies@berkeley.edu>
2011-09-24 15:12:28 +0000

    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@68 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- Level1.py
+++ Level1.py
@@ -3,7 +3,7 @@ from block import Block
 from player import Player
 from terrain import Terrain
 from Level import Level
-from monster import Monster
+#from monster import Monster
 
 class TutLevel(Level):
     def __init__(self, width, height):
@@ -13,7 +13,7 @@ class TutLevel(Level):
         boxleft= Terrain(-1,5,2,10,0,0,100,self,0.5)
         boxbottom= Terrain(20,9,40,2,0,0,100,self,0.5)
         boxright= Terrain(41,5,2,10,0,0,100,self,0.5)
-        mon= Monster(4,7,1,2,0,0,1,self,.5,3,0)
+        #mon= Monster(4,3,1,2,0,0,1,self,.5,3,0)
         b =   Block(20, 5,  2, 2, 0, 0,  1, self, 0.5)
         c =  Player(15, 5,  1, 2, 0, 0,  1, self, 0.5)
         d =   Block(27, 5, 2, 2, 0, 5,  2, self, 0.5)  
@@ -24,5 +24,5 @@ class TutLevel(Level):
         self.add_elem(boxright)
         self.add_elem(boxbottom)
         self.add_elem(boxleft)
-        self.add_elem(mon)
+        #self.add_elem(mon)
 
