776c4455513ccd1caad45bfc914424cac60bc45e
Peterfly <peterqian@hotmail.com>
2011-09-24 19:40:41 +0000

    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@103 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- LevelReal.py
+++ LevelReal.py
@@ -1,30 +1,54 @@
 from elements import Element
 from block import Block
-from player import Player  
-from Level import Level
+from player import Player
 from terrain import Terrain
+from Level import Level
+from monster import Monster
+from Goal import Goal
+from GoalBlock import GoalBlock
+from Spike import Spike
 
 class TutLevel(Level):
     def __init__(self, width, height):
         super(TutLevel, self).__init__(width, height)
         ##initialize blocks##
-        p1 =     Terrain(70, 9, 40, 2, 0, 0,100, self, 0.5)
-        p2=     Terrain(20,1,40,2,0,0,100,self,0.5)
-        p3=     Terrain(70,1,40,2,0,0,100,self,.5)
-        p4=     Terrain(-10,-20,2,41,0,0,100,self,.5)
-        p5=     Terrain(20,-40,40,2,0,0,100,self,0.5)
-        p6=     Terrain(70,-40,40,2,0,0,100,self,.5)
-        p7=     Terrain(45,20,10,2,0,0,100,self,.5)
-        b =   Block(70, -45,  2, 2, 0, 0,  1, self, 0.5)
-        c =  Player(70, 5,  1, 2, 0, 0,  1, self, 0.5)
-        d =   Block(7, -45, 2, 2, 0, 5,  2, self, 0.5)  
+        boxtop = Terrain(50, 2, 100, 2, 0, 0,100, self, 0.5)
+        boxleft= Terrain(1,50,2,100,0,0,100,self,0.5)
+        boxbottom= Terrain(50,100,100,2,0,0,100,self,0.5)
+        boxright= Terrain(100, 50,2,100,0,0,100,self,0.5)
+        goal= Goal(95,95,1,5,0,0,100,self,0.5)
+        a =   GoalBlock(4, 90, 2, 2, 0, 5,  2, self, 0.5)
+        
+        t1 = Terrain(8, 70, 2, 56, 0, 0, 100, self, 0.5)
+        c =  Player(3, 90,  1, 2, 0, 0,  1, self, 0.5)
         self.add_elem(c)
-        self.add_elem(b)
-        self.add_elem(d)
-        self.add_elem(p1)
-        self.add_elem(p2)
-        self.add_elem(p3)
-        self.add_elem(p4)
-        self.add_elem(p5)
-        self.add_elem(p6)
-        self.add_elem(p7)
+        for i in range (0, 10):
+            e = Spike(15+i, 98, 1, 1, 0, 0, 100, self, 0.5)
+            self.add_elem(e)
+
+        for i in range (0, 10):
+            e = Spike(35+i, 98, 1, 1, 0, 0, 100, self, 0.5)
+            self.add_elem(e)
+
+        for i in range (0, 10):
+            e = Spike(40+i, 98, 1, 1, 0, 0, 100, self, 0.5)
+            self.add_elem(e)
+
+        b = Block (4, 90, 2, 2, 0, 5, 2, self, 0.5)
+
+        c =  Player(3, 90,  1, 2, 0, 0,  1, self, 0.5)
+
+        
+        
+        
+
+
+
+        
+        self.add_elem(t1)
+        self.add_elem(boxtop)
+        self.add_elem(boxright)
+        self.add_elem(boxbottom)
+        self.add_elem(boxleft)
+        self.add_elem(a)
+        self.add_elem(goal)
