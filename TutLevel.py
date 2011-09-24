51ba7005093cf1fccb50585cd20213b59c6aa82f
Ajay Tripathi <chausies@berkeley.edu>
2011-09-24 08:25:45 +0000

    Updated TutLevel, fixing a bug and adding a second block that starts out going upwards.
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@18 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- TutLevel.py
+++ TutLevel.py
@@ -8,10 +8,12 @@ class TutLevel(Level):
     def __init__(self, width, height):
         super(TutLevel, self).__init__(width, height)
         ##initialize blocks##
-        a = Terrain(5, 5, 10, 1, 0, 0, 10, self, 0.5)
-        b =     Box(5, 0,  1, 1, 0, 0,  1, self, 0.5)
+        a = Terrain(5, 5, 20, 1, 0, 0, 10, self, 0.5)
+        b =   Block(5, 0,  1, 1, 0, 0,  1, self, 0.5)
         c =  Player(0, 0,  1, 2, 0, 0,  1, self, 0.5)
+		d =   Block(10, 0, 2, 2, 0, 1,  2, self, 0.5)  
         self.add_elem(c)
         self.add_elem(a)
         self.add_elem(b)
+		self.add_elem(d)
 
