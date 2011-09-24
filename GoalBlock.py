487578d7bc4223e2561dfc9b3bf4726fa00290b7
Keegan Mann <keeganmann@gmail.com>
2011-09-24 18:27:57 +0000

    added blue texture
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@93 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- GoalBlock.py
+++ GoalBlock.py
@@ -1,5 +1,7 @@
 from block import Block
+from TiledGraphics import TiledGraphics
 
 class GoalBlock(Block):
     def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co):
         super(GoalBlock, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
+        self.graphics = TiledGraphics("img/blue.png", 40, 40)
