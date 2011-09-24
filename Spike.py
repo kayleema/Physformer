31ea1d8bafe6d6fd2d26b49a55cafb386fb5cfee
Keegan Mann <keeganmann@gmail.com>
2011-09-24 19:08:45 +0000

    fixed s[pikes to tile graphics
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@98 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- Spike.py
+++ Spike.py
@@ -2,11 +2,12 @@ from elements import Element
 from terrain import Terrain
 from player import Player
 from ImageGraphics import ImageGraphics
+from TiledGraphics import TiledGraphics
 
 class Spike(Terrain):
      def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co):
          super(Spike, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
-         self.graphics = ImageGraphics("img/spike.png")
+         self.graphics = TiledGraphics("img/spike.png", 40, 40)
      def collide(self, elem):
          super(Spike, self).collide(elem)
          if isinstance(elem, Player):
