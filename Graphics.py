e0076b559fce38c36b628f85184dce6b410beed4
Keegan Mann <keeganmann@gmail.com>
2011-09-24 05:28:25 +0000

    Removed an Empty file and added the Graphics class
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@9 7638e4d6-f48f-4d7e-a304-0c041ff8053c

---
+++ Graphics.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Graphics():
+    def __init__(self, element):
+        self.color = (0, 255, 0)
+        self.elem = element
+    def draw(screen, x, y, w, h):
+        """draws a rectangle representation of the element defined in the
+        constructor
+        """
+        pygame.draw.rect(screen,color,[x,y,w,h],2)
