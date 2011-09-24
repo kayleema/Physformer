66f276eb0f313fa4c56af5bd6397e93cc74a1935
Peterfly <peterqian@hotmail.com>
2011-09-24 13:42:37 +0000

    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@55 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- monster.py
+++ monster.py
@@ -7,14 +7,14 @@ class Monster(Terrain):
 
     def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co, dx, dy):
         super(Monster, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
-        self.x_lim, self.y_lim = x1, y1
+        self.x_lim, self.y_lim = dx, dy
         self.vx = self.run_velocity
         self.vy = self.jump_velocity
         self.x_ori = self.x
         self.y_ori = self.y
 
     def sim(self, time):
-        if abs(self.x_ori - self.x) > dx:
+        if abs(self.x_ori - self.x) > self.x_lim:
             vx = -vx
         super(Monster, self).sim(time)
 
