8b3efb50fedb1efead4ff8af40b46db193f856f2
Peterfly <peterqian@hotmail.com>
2011-09-24 16:21:16 +0000

    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@82 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- elements.py
+++ elements.py
@@ -3,6 +3,7 @@ from Graphics import Graphics
 """
 
 class Element(object):
+    self.exist = True
     def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, level_co, c_co):
         """
         initialize everything
@@ -11,7 +12,7 @@ class Element(object):
         vx_co, vy_co: speed to 2 coordinates.
         c_co: coefficient of restitution
         """
-        self.exist = True
+        
         self.w = w_co
         self.h = h_co
         self.vx = vx_co
