64081e65fe9119eaf1619464725ca7e9ba6b031e
Keegan Mann <keeganmann@gmail.com>
2011-09-24 15:57:15 +0000

    better graphics
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@73 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- player.py
+++ player.py
@@ -16,7 +16,7 @@ class Player(Element):
         self.movedown = False
         self.health = 100
         self.grounded = False
-        self.graphics = ImageGraphics("img/horn.png")
+        self.graphics = ImageGraphics("img/charac.png")
 
     def sim(self, time):
         #print("Player Position: ", self.x, self.y)
