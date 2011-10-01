657d5f30b8724ab345bf4cd31ffb88b96d7e4698
Keegan Mann <keeganmann@gmail.com>
2011-10-01 03:17:08 +0000

    rounded the corners of blue blocks
    git-svn-id: http://keegan.no-ip.biz/svn/hack/trunk@126 7638e4d6-f48f-4d7e-a304-0c041ff8053c

--- GoalBlock.py
+++ GoalBlock.py
@@ -4,4 +4,4 @@ from TiledGraphics import TiledGraphics
 class GoalBlock(Block):
     def __init__(self, x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co):
         super(GoalBlock, self).__init__(x_co, y_co, w_co, h_co, vx_co, vy_co, mass, world_name, c_co)
-        self.graphics = TiledGraphics("img/blue.png", self.mylevel.meter*self.w, self.mylevel.meter*self.h)
+        self.graphics = TiledGraphics("img/bluerounded.png", self.mylevel.meter*self.w, self.mylevel.meter*self.h)
